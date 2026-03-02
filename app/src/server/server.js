fea657d93dd3d2c0fde37614b8b75643833a543d
christiano0407 <placebo0704@gmail.com>
2026-03-02 15:16:11 -0600

    [docker compose] Add new file to run Docker Compose and Run Images Nginx Proxy

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -5,16 +5,22 @@ import { fileURLToPath } from "url";
 const app = express(); 
 const PORT = 3000; 
 
-// = _dirname = 
+// = _dirname in ES Modules = 
 const __filename = fileURLToPath(import.meta.url); 
 const __dirname = path.dirname(__filename); 
 
+// === Environment Variable ===
+const INSTANCE = process.env.INSTANCE_NAME || "unknown"; 
+
+// === Static Files (Only Files Public) ===
 app.use(express.static(path.join(__dirname, '../')));
 
+// === Fullback HTML ===
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../html/index.html")); 
 }); 
 
+// === PORT | Server ===
 app.listen(PORT, () => {
   console.log(`Express running on port ${PORT}`);
 });
